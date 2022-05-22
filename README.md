7b0a34fe7d43ff66a2d5ba478c705a042f5270a3
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-05-22 17:33:45 -0400

    Done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# AirBnB Front-end clone with React
+![alt text](./public/Screen%20Shot%202022-05-22%20at%205.27.56%20PM.png)
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+import Map, {Marker} from 'react-map-gl';
 import './App.css';
+import Flat from './components/flat';
+// import Marker from './components/marker';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './components/marker.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        flats: [],
+        allFlats: [],
+        selectedFlat: null,
+        search: ""
+      };
+    }
+
+    componentDidMount(){
+        const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+        fetch(url)
+            .then(response => response.json())
+            .then ((data) => {
+                this.setState({
+                    flats: data,
+                    allFlats: data
+                })
+            })
+    }
+
+    selectFlat = (flat) =>{
+        this.setState({
+            selectedFlat: flat
+        })
+    }
+
+    handleSearch = (event) =>{
+        this.setState({
+            search:event.target.value,
+            flats:this.state.allFlats.filter((flat) => new RegExp(event.target.value, "i").exec(flat.name))
+        })
+    }
+
+render(){
+    // let center = {
+    //     latitude: 48.8566,
+    //     longitude: 2.3522
+    //   };
+
+    const MAPBOX_TOKEN = 'pk.eyJ1IjoiYW5ka2luZyIsImEiOiJjbDB2bHZ2cjIwd2RkM3BvYWFsbjJqdG9vIn0.MxNTMNUhZnNvhm-sKJnHmw';
+    let classes = "marker";
+    if(this.props.selected){
+        classes += "selected";
+    }
+   return (
+    <div className = "app">
+        <div className = "main">
+            <div className = "search">
+            <input
+                type="text"
+                placeholder="Search..."
+                value={this.state.search}
+                onChange={this.handleSearch} />
+            </div>
+            <div className = "flats">
+                {this.state.flats.map((flat) => {
+                    return <Flat key={flat.name}
+                    flat={flat}
+                    selectFlat={this.selectFlat} />
+                })}
+            </div>
+        </div>
+            <div className = "map">
+            <Map
+                initialViewState={{
+                    latitude: 48.8566,
+                    longitude: 2.3522,
+                    zoom: 11
+                }}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                mapboxAccessToken={MAPBOX_TOKEN}
+                >
+                {this.state.flats.map((flat) => {
+                    return <Marker key={flat.name}
+                                   latitude = {flat.lat}
+                                   longitude = {flat.lng}
+                                   text = {flat.price}
+                                   selected = {flat === this.state.selectedFlat}>
+
+                        <div className={classes}>{flat.price}</div>
+                    </Marker>})}
+
+                </Map>
+            </div>
     </div>
   );
 }
-
+}
 export default App;
